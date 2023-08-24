fd098ccce57b3ffd1ad74656ce52f2d41dd8cb23
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-08-24 13:24:41 -0700

    Update WonderBus.py

--- WonderBus.py
+++ WonderBus.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from datetime import datetime, timedelta
 
 
 st.set_page_config(page_title= "Profit/Loss Report", layout= "wide")
@@ -35,17 +34,7 @@ event = st.sidebar.multiselect(
 
 if "All" in event:
     event = df["Event"].unique().tolist()
-'''
-start_date = datetime(2020, 1, 1)
-end_date = start_date + timedelta(weeks = 1)
-
-sell_date = st.sidebar.slider(
-    label = "Select Ticket Sale Date",
-    min_value = start_date, 
-    max_value = end_date,
-    value = (start_date, end_date),
-    step = timedelta(days = 1))
-'''
+    
 df_selection = df.query("Customer == @customer & Venue == @venue & Event == @event")
 
 
@@ -60,7 +49,7 @@ st.markdown("##")
 net_sales = round(df_selection["Profit Loss"].sum(), 2)
 average_roi = round(df_selection["ROI(%)"].mean(), 2)
 tot_quant_sold = df_selection["Sold Qty"].sum()
-quant_sold_by_date = df_selection.groupby(by = ["Sale Date"]).sum()[["Sold Qty"]].sort_values(by = "Sold Qty")
+#quant_sold_by_date = df_selection.groupby(by = ["Sale Date"]).sum()[["Sold Qty"]].sort_values(by = "Sold Qty")
 
 #first two columns
 left_column, right_column = st.columns(2)
@@ -71,9 +60,8 @@ with right_column:
     st.subheader("Quantity Sold:")
     st.subheader(f"{tot_quant_sold:,}")
 
-st.markdown("##")
-
 #bar chart
+'''
 fig_ticket_sales = px.bar(quant_sold_by_date, 
                           x = quant_sold_by_date.index, 
                           y = "Sold Qty", 
@@ -83,5 +71,5 @@ fig_ticket_sales = px.bar(quant_sold_by_date,
                           template = "plotly_white")
 
 st.plotly_chart(fig_ticket_sales)
-
+'''
 st.markdown("---")
