0cbc61572fc7616ddf66c2b5172e4bd1b53ff176
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-09-27 12:32:25 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,15 +6,12 @@ st.write("# Welcome! 👋")
 
 st.markdown("##")
 
-def logged_in():
 name = st.text_input('Password')
 
 if name == st.secrets["password"]:
     st.session_state['state'] = 1
-    logged_in = True
 else:
     st.session_state['state'] = 2
-    logged_in = False
  
 if st.session_state['state'] == 1:
     st.write('Success')
