3dd4c65ab19b5c658d0db2569ba74a5a96a1648d
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-09-27 12:27:14 -0700

    Update WonderBus.py

--- pages/WonderBus.py
+++ pages/WonderBus.py
@@ -2,6 +2,10 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+if not logged_in():
+    st.warning("You must log-in to see the content of this sensitive page! Head over to the log-in page.")
+    st.stop()  # App won't run anything after this line
+
 st.set_page_config(page_title= "Profit/Loss Report", layout= "wide")
 
 #convert first columns to strings
