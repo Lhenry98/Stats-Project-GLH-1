f8071ecc41d76115ccf8b6993c1ab453425266fd
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-09-27 10:46:48 -0700

    Update WonderBus.py
    attempt 2

--- pages/WonderBus.py
+++ pages/WonderBus.py
@@ -38,8 +38,7 @@ if "All" in event:
 df_selection = df.query("Customer == @customer & Venue == @venue & Event == @event")
 df_selection["Sale Date"] = df_selection["Sale Date"].str[:-5]
 df_selection["Sale Date"] = df_selection["Sale Date"] + "00:00"
-df_selection["Invoice#"] = df_selection["Invoice#"].style.format(thousands = "")
-df_selection["Ex. Order No"] = df_selection["Ex. Order No"].style.format(thousands = "")
+df_selection.style.format(thousands = "")
 quant_sold_by_date = df_selection.groupby(by = ["Sale Date"]).sum()[["Sold Qty"]].sort_values(by = "Sold Qty")
 
 st.dataframe(df_selection,
