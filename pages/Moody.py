08e8e316fafd812c072309bcb57e34993f486151
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-09-27 16:31:16 -0700

    Update Moody.py

--- pages/Moody.py
+++ pages/Moody.py
@@ -102,7 +102,9 @@ fig_ticket_sales = px.bar(quant_sold_by_date,
                           orientation = "v",
                           title = "<b>Quantity Sold by Date<b>",
                           color_discrete_sequence=['#ec7c34'] * len(quant_sold_by_date),
-                          template = "plotly_white")
+                          template = "plotly_white"
+                         )
+fig_ticket_sales.update_traces(marker.line.width = 0)
 
 st.plotly_chart(fig_ticket_sales)
 
