6b1e14caa5096dead6104e58a0e2669cfdf54cee
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-09-27 10:45:31 -0700

    Update WonderBus.py
    ditch thousand separator

--- pages/WonderBus.py
+++ pages/WonderBus.py
@@ -15,7 +15,6 @@ customer = st.sidebar.multiselect(
     label = "Select the Customer:",
     options = ["All"] + df["Customer"].unique().tolist(),
     default = "All")
-
 if "All" in customer:
     customer = df["Customer"].unique().tolist()
 
@@ -24,7 +23,6 @@ venue = st.sidebar.multiselect(
     label = "Select the Venue:",
     options = ["All"] + df["Venue"].unique().tolist(),
     default = "All")
-
 if "All" in venue:
     venue = df["Venue"].unique().tolist()
     
@@ -32,16 +30,18 @@ event = st.sidebar.multiselect(
     label = "Select the Event Date:",
     options = ["All"] + df["Event"].unique().tolist(),
     default = "All")
-
 if "All" in event:
     event = df["Event"].unique().tolist()
 
+
+# ---- MAIN PAGE ----
 df_selection = df.query("Customer == @customer & Venue == @venue & Event == @event")
 df_selection["Sale Date"] = df_selection["Sale Date"].str[:-5]
 df_selection["Sale Date"] = df_selection["Sale Date"] + "00:00"
+df_selection["Invoice#"] = df_selection["Invoice#"].style.format(thousands = "")
+df_selection["Ex. Order No"] = df_selection["Ex. Order No"].style.format(thousands = "")
 quant_sold_by_date = df_selection.groupby(by = ["Sale Date"]).sum()[["Sold Qty"]].sort_values(by = "Sold Qty")
 
-# ---- MAIN PAGE ----
 st.dataframe(df_selection,
              column_config = {
                  "Profit Loss": None,
@@ -75,7 +75,7 @@ fig_ticket_sales = px.bar(quant_sold_by_date,
                           x = quant_sold_by_date.index,
                           y = "Sold Qty",  
                           orientation = "v",
-                          title = "<b>Quantity Sold by Date<b>",
+                          title = "<b>Quantity Sold by Date (Hourly)<b>",
                           color_discrete_sequence=['#ec7c34'] * len(quant_sold_by_date),
                           template = "plotly_white")
 
