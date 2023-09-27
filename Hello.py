fafb8f5fe595b77db9230e748ee897c8cb5c9d98
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-09-27 12:30:21 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -11,10 +11,10 @@ name = st.text_input('Password')
 
 if name == st.secrets["password"]:
     st.session_state['state'] = 1
-    logged_in() = True
+    logged_in = True
 else:
     st.session_state['state'] = 2
-    logged_in() = False
+    logged_in = False
  
 if st.session_state['state'] == 1:
     st.write('Success')
