67153788f9c3b6e34302be19f156dc8622af81cf
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-10-02 12:41:26 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,15 +7,14 @@ st.write("# Welcome! 👋")
 st.markdown("##")
 
 name = st.text_input('Password')
-session_state = 0
 
 #add for each login
 if name == st.secrets["password"]:
-    session_state = 1
+    st.session_state['state'] = 1
 else:
-    session_state = 2
+    st.session_state['state'] = 2
 #-------------------------------------
-if session_state == 1:
+if st.session_state['state'] == 1:
     st.write('Success, you are free to browse!')
 else:
     st.write("Enter Password")
