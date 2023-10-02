7f0d17ae8cd89d79ffcc43dee2561c1bd3469a6b
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-10-02 13:05:27 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,10 +13,14 @@ if 'key' not in st.session_state:
 
 if name == st.secrets["soldout"]["soldout"]:
     st.session_state.key = 1
-else:
+elif name == st.secrets["moody"]["moody"]:
     st.session_state.key = 2
+else:
+    st.session_state.key = 3
 #-------------------------------------
 if st.session_state.key == 1:
+    st.write('Success, you are free to browse!')
+elif st.session_state.key == 2:
     st.write('Success, you are free to browse!')
 else:
     st.write("Enter Password")
