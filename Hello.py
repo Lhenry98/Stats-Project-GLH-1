fed77c6a6d0de92af3a574550f028e922d388d66
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-10-02 11:54:48 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -11,12 +11,16 @@ name = st.text_input('Password')
 #st.session_state['state'] = 0
 
 #add for each login
-if name == st.secrets["password"]:
+if name == st.secrets["admin"]:
     st.session_state['state'] = 1
+if name == st.secrets["moody"]:
+    st.session_state['state'] = 2
+if name == st.secrets["zilker"]:
+    st.session_state['state'] = 3
 else:
     st.session_state['state'] = 0
 #-------------------------------------
-if st.session_state['state'] == 1:
+if st.session_state['state'] != 0:
     st.write('Success, you are free to browse!')
 else:
     st.write("Enter Password")
