6456400b7621f06e7144f3f2e72c9e08eaa2127a
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2023-10-02 12:24:36 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -9,7 +9,7 @@ st.markdown("##")
 name = st.text_input('Password')
 
 #add for each login
-if name == st.secrets["password"] or name == st.secrets["moody"]:
+if name == st.secrets["password"]:
     st.session_state['state'] = 1
 else:
     st.session_state['state'] = 2
