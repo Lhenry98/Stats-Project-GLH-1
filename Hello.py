e813c99ca3170b1d203e3d28ee68194ccabc837a
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2024-01-17 18:19:33 -0800

    Update Hello.py
    trying to hide visibility via 1 year-old youtube comment.

--- Hello.py
+++ Hello.py
@@ -2,18 +2,15 @@ import streamlit as st
 
 st.set_page_config(page_title="Welcome")
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            .css-1rs6os {visibility: hidden;}
+            .css-17ziqus {visibility: hidden;}
+            """
+st.markdown(hide_st_style,unsafe_allow_html=True)
             
 st.write("# Welcome! 👋")
 
