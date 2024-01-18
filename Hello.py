44df2bb134e079beec5c50ea5a04020d8662a4cb
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2024-01-17 18:17:05 -0800

    Update Hello.py
    last try didn't work, but the same forum says that this solution might work.

--- Hello.py
+++ Hello.py
@@ -2,14 +2,18 @@ import streamlit as st
 
 st.set_page_config(page_title="Welcome")
 
-hide_st_style = """
-            <style>
-            #GithubIcon {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-
-st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
             
 st.write("# Welcome! 👋")
 
