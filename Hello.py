9adaf7fc10e4505655fd58ffeefd49d26b3429c0
Lhenry98 <143128153+Lhenry98@users.noreply.github.com>
2024-01-17 17:53:21 -0800

    Update Hello.py
    testing if previous change hid things properly

--- Hello.py
+++ Hello.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 st.set_page_config(page_title="Welcome")
-
-hide_st_style = """
+"""
+hide_st_style = 
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
-            """
+"""
 
-st.markdown(hide_st_style, unsafe_allow_html=True)
+#st.markdown(hide_st_style, unsafe_allow_html=True)
             
 st.write("# Welcome! 👋")
 
